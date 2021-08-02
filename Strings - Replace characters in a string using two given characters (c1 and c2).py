6134b8cde0d241c2839bd42e5634aa6d6c5bab15
Aishwarya Girish Menon <aishwaryagm1999@gmail.com>
2021-08-03 02:54:20 +0530

    Add files via upload
    Strings - Replace characters in a string using two given characters (ch1 and ch2) (Eg: replace char ch1 with char ch2 and char ch2 with char ch1)

---
+++ Strings - Replace characters in a string using two given characters (c1 and c2).py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # PYTHON COMMON CHARACTERS OF TWO STRINGS IN ALPHABETICAL ORDER
+
+# In[ ]:
+
+
+
+
+
+# In[15]:
+
+
+str1 = str(input("Enter Main String: "))
+ch1 = str(input("Enter value of ch1: "))
+ch2 = str(input("Enter value of ch2: "))
+str2 = "" #string variable to have final string
+
+for i in range(len(str1)): 
+    if str1[i] == ch2:        #replacing ch2 with ch1 from str1 and adding to final string
+        str2 = str2+ch1
+        
+    elif str1[i] == ch1:      #replacing ch1 with ch2 from str1 and adding to final string
+        str2 = str2+ch2
+        
+    else:
+        str2 = str2+str1[i]   #adding remaining characters to final string
+        
+print(str2)
+
+
+# In[ ]:
+
+
+
+
